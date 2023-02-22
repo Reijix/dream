afae18e8e7c3ed8bb9d81e4f8da7c72d9729ed14
Leon Vatthauer <leon.vatthauer@fau.de>
2023-02-22 08:54:52 +0100

    Added IRSyntax

---
+++ IR/IRSyntax.hs
@@ -0,0 +1,68 @@
+module IRSyntax where
+
+import Symbol ( Type )
+
+data IRConstant
+    = IRIntConstant Int
+    | IRRealConstant Double
+
+data IRVariable = IRVar {
+    varName :: String,
+    varGlobal :: Bool,
+    varVirtualRegister :: Bool
+    }
+
+data IROperand
+    = IRVariable IRVariable
+    | IRConstant IRConstant
+
+data IRFunction = IRFunction {
+    funName :: String,
+    funRetType :: Type,
+    funInstructions :: [IRInstruction],
+    funParameters :: [IRVariable],
+    funLocalVars :: [IRVariable],
+    funVirtualRegs :: [IRVariable]
+}
+
+data IRProgram = IRProgram {
+    progGlobalVars :: [IRVariable],
+    progFunction :: [IRFunction]
+}
+
+data IRInstruction
+    = Jump Jump
+    | Assignment Assignment
+    | NOP
+    | LABEL
+    | STORE
+    | RET
+
+data Assignment
+    = BinaryOperation BinaryOperation
+    | CastOperation CastOperation
+    | MOV
+    | LOAD
+    | CALL
+
+data CastOperation
+    = I2R
+    | R2I
+
+data BinaryOperation
+    = ADD
+    | SUB
+    | MUL
+    | DIV
+
+data Jump 
+    = ConditionalJump ConditionalJump
+    | JMP
+
+data ConditionalJump
+    = JEQ
+    | JNE
+    | JLE
+    | JLT
+    | JGT
+    | JGE
