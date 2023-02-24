331223ffa873f461b83275aed1394ab9e875bed4
Leon Vatthauer <leon.vatthauer@fau.de>
2023-02-24 19:19:16 +0100

    Improved error message

--- analysis/AnalysisError.hs
+++ analysis/AnalysisError.hs
@@ -5,4 +5,7 @@ import Text.Parsec (SourcePos)
 data AnalysisError
   = NameError SourcePos String
   | TypeError SourcePos String
-  deriving (Show)
+
+instance Show AnalysisError where
+  show (NameError sourcePos err) = "Encountered error during nameanalysis:\n" ++ show err ++ "\nat: " ++ show sourcePos
+  show (TypeError sourcePos err) = "Encountered error during typeanalysis:\n" ++ show err ++ "\nat: " ++ show sourcePos
